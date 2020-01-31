10f3f02d636dfa61be1e1e10034c73ff2a97d891
Yuan <yuant614@gmail.com>
2020-01-31 21:14:58 +0800

    added comment of url connectivity check function

--- app/service/utility.ts
+++ app/service/utility.ts
@@ -5,7 +5,11 @@ export default class Utility extends Service {
 
   private URL_PATTERN = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
 
-
+  /**
+   * check the connectivity of a given URL, if http prefix is not assigned, http:// would be append as prefix of the URL
+   * @param url given website address
+   * @returns {Promise<any>}
+   */
   public async checkURLConnectivity(url): Promise<any> {
     return new Promise((resolve, reject) => {
       const logger = this.ctx.logger;
